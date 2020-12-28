90925c147330e7e6512032896610c4195ca3c0cf
Mrml91 <theophile.mongin@gmail.com>
2020-12-28 15:17:30 +0100

    fix h5connections

--- additional_features/make_features.py
+++ additional_features/make_features.py
@@ -8,8 +8,10 @@ from additional_features.time_features import _create_time_features
 
 
 
-def make_all_features(h5_train=None, h5_test=None, overwrite=False, verbose=True, n_chunks=100):
-
+def make_all_features(overwrite=False, verbose=True, n_chunks=100):
+    """
+    To run before h5
+    """
     train_file = "./kaggle_data/X_train.h5/X_train.h5"
     test_file = "./kaggle_data/X_test.h5/X_test.h5"
     h5_train = h5py.File(train_file, mode='a')
