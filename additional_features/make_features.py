77fa3bf63b2b0d07006b75b86dff69d080629b39
Mrml91 <theophile.mongin@gmail.com>
2020-12-28 15:19:54 +0100

    fix h5connections

--- additional_features/make_features.py
+++ additional_features/make_features.py
@@ -8,19 +8,16 @@ from additional_features.time_features import _create_time_features
 
 
 
-def make_all_features(overwrite=False, verbose=True, n_chunks=100):
-    """
-    To run before h5
-    """
-    train_file = "./kaggle_data/X_train.h5/X_train.h5"
-    test_file = "./kaggle_data/X_test.h5/X_test.h5"
-    h5_train = h5py.File(train_file, mode='a')
-    h5_test = h5py.File(test_file, mode='a')
+def make_all_features(h5_train, h5_test, overwrite=False, verbose=True, n_chunks=100):
+    # train_file = "./kaggle_data/X_train.h5/X_train.h5"
+    # test_file = "./kaggle_data/X_test.h5/X_test.h5"
+    # h5_train = h5py.File(train_file, mode='a')
+    # h5_test = h5py.File(test_file, mode='a')
 
     for create_func in [_create_log_energy, _create_log_modulus, _create_pulse_max_log_energy_and_freq,
                         _create_speed_and_acceleration, _create_time_features]:
         for h5_file in [h5_train, h5_test]:
             create_func(h5_file, n_chunks=n_chunks, overwrite=overwrite, verbose=verbose)
 
-    h5_train.close()
-    h5_test.close()
+    # h5_train.close()
+    # h5_test.close()
